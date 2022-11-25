9ebf7b526ee8675fa4ebf64e7a129682f4a20a73
Csabai Máté Mihály <csabai.m.88@gmail.com>
2022-11-25 11:38:50 +0100

    cors delete

--- backend.py
+++ backend.py
@@ -8,26 +8,12 @@ from elasticsearch import helpers
 
 
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from pydantic import BaseModel
 
 # ML
 from transformers import pipeline
 import spacy
-
-app = FastAPI()
-
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 
 logging.basicConfig(
     level=logging.INFO,
